926c365c1c5a40c6a5a6bba79a314b84114ac60e
Patrick <malamashangala13@gmail.com>
2022-08-01 19:03:36 +0200

    add project page

--- server.js
+++ server.js
@@ -71,6 +71,12 @@ app.get('/about',(req,res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects'
+	});
+});
+
 
 app.listen(port, () => {
 	console.log(`server is up on port ${port}`)
